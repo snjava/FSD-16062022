9b7bf08939062dac805bb52135bb101d7477b2cf
snjava <61556337+snjava@users.noreply.github.com>
2022-10-16 09:25:40 +0530

    Package, Import and Access Modifier

--- code/corejava/workspace/Keyword/src/institute/student/StudentDetails.java
+++ code/corejava/workspace/Keyword/src/institute/student/StudentDetails.java
@@ -2,9 +2,30 @@ package institute.student;
 
 public class StudentDetails {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int id;
+	public String name;
+	String address;
+	public String contact;
+	
+	public void studProgress() {
+		System.out.println("Student Progress....");
 	}
+	
+}
 
+
+class StudentBatch extends StudentDetails {
+	public void studDetailsByObject() {
+		StudentDetails dtl = new StudentDetails();
+		System.out.println(dtl.name);
+		System.out.println(dtl.address);
+		System.out.println(dtl.contact);
+		
+	}
+	
+	public void studDetailsByInheritance() {
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(contact);
+	}
 }
--- code/corejava/workspace/Keyword/src/institute/teacher/TeachDetails.java
+++ code/corejava/workspace/Keyword/src/institute/teacher/TeachDetails.java
@@ -1,10 +1,24 @@
 package institute.teacher;
 
+//import institute.student.*; // all the classes from the student package
+import institute.student.StudentDetails; // single class from the student package
+import java.util.Scanner;
+
 public class TeachDetails {
 
 	public static void main(String[] args) {
+		TeachDetails dtl = new TeachDetails();
+		dtl.getStudDtl();
 		
-
+		Scanner scan = new Scanner(System.in);
+	}
+	
+	public void getStudDtl() {
+		StudentDetails studDtl = new StudentDetails();
+		System.out.println(studDtl.name);
+		System.out.println(studDtl.contact);
+		
+		studDtl.studProgress();
 	}
 
 }
