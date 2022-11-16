d6045f04f04738c89080ee06ef9d94079f46e167
snjava <61556337+snjava@users.noreply.github.com>
2022-11-16 10:28:02 +0530

    LInkedList and Hash Algo
    LInkedList and Hash Algo

--- code/corejava/workspace/Collection/src/LinkedListDemo.java
+++ code/corejava/workspace/Collection/src/LinkedListDemo.java
@@ -9,9 +9,32 @@ public class LinkedListDemo {
 		list.add(50.1);
 		list.add("Abc");
 		
-		System.out.println(list);
+		System.out.println("Original List : " + list);
+		
+		list.push(111);
+		System.out.println("After Push : " +list);
+		
+		// it selects and remove 1st element, if 1st element is not present then it will
+		// throw an Exception
+		System.out.println("Pop Element : " + list.pop());
+		System.out.println("After pop : " +list);
+		
+		list.addFirst(333);
+		list.addLast("ZZZ");
+		System.out.println("add first & last : " +list);
+		
+		list.removeFirst();
+		list.removeLast();
+		System.out.println("remove first & last : " +list);
 		
 		
+		System.out.println("Peek Element : " + list.peek());
+		System.out.println("After peek : " +list);
+		
+		// it selects and remove 1st element, if 1st element is not present then it will
+		// return null
+		System.out.println("poll Element : " + list.poll());
+		System.out.println("After poll : " +list);
 	}
 
 }
