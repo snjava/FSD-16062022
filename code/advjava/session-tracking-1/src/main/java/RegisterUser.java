ecab851f4cb8460001362d94a61a042c1e1af0cc
scn-tech <scnaikwadi2@gmail.com>
2022-12-17 10:17:54 +0530

    Database Connection
    Database Connection

--- code/advjava/session-tracking-1/src/main/java/RegisterUser.java
+++ code/advjava/session-tracking-1/src/main/java/RegisterUser.java
@@ -1,6 +1,10 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,17 +17,47 @@ public class RegisterUser extends HttpServlet {
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String contact = request.getParameter("contact");
-		
 		String city = request.getParameter("city");
 		String pincode = request.getParameter("pin");
 		String state = request.getParameter("state");
-		
-		System.out.println("Name : " + name);
-		System.out.println("Email : " + email);
-		System.out.println("COntact : " + contact);
-		System.out.println("City : " + city);
-		System.out.println("Pincode : " + pincode);
-		System.out.println("State : " + state);
+		int code=0;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fsd16aug","root","root");
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO employee values(0,?,?,?,?,?,?)");
+				stmt.setString(1, name);
+				stmt.setString(2, email);
+				stmt.setString(3, contact);
+				stmt.setString(4, city);
+				stmt.setString(5, pincode);
+				stmt.setString(6, state);
+			int cnt = stmt.executeUpdate();
+			con.close();
+
+			if(cnt>0) {
+				code=1;
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	
+		response.sendRedirect("user-info.jsp?c="+code);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- code/advjava/session-tracking-1/src/main/webapp/user-info.jsp
+++ code/advjava/session-tracking-1/src/main/webapp/user-info.jsp
@@ -10,6 +10,27 @@
 	<div align="center">
 		<h1>Step 1 of 3</h1>
 		<hr>
+		
+		<%
+			String code = request.getParameter("c");
+			String msg = "";
+			String color="";
+			if(code!=null) {
+				switch(code) {
+					case "1":
+						msg = "Your data has been save successfully.....";
+						color= "green";
+						break;
+					case "0":
+						msg = "Your Registration is failed. Please try again.!!";
+						color= "red";
+						break;
+				}
+			}
+		%>
+		
+		<h3 style="color: <%=color%>"> <%=msg %></h3>
+
 		<h2><u>User Personal Details</u></h2>
 		<form action="user-address.jsp">
 			Enter Name : <input type="text" name="name">
